c5ac6a8c3ff2af84ed7d78c9a828b28909878549
Compdog Inc <66779418+Compdog-inc@users.noreply.github.com>
2023-09-19 08:45:48 -0400

    Update rasterizer.js

--- rasterizer.js
+++ rasterizer.js
@@ -48,9 +48,9 @@ window.Utils['_register']('Rasterizer', function(factory){
     const drawTriangles = function (vertexShader,geoShader,pixelShader,matrix, verts, tris, viewX, viewY, viewWidth,viewHeight,viewNear,viewFar) {
       const ctdata = ctdata_ref();
       for (let t = 0; t < tris.length / 3; ++t) {
-        const p0=vertexShader(verts[tris[t*3+0]]);
-        const p1=vertexShader(verts[tris[t*3+1]]);
-        const p2=vertexShader(verts[tris[t*3+2]]);
+        const p0=vertexShader([verts[tris[t*3+0]*3+0],verts[tris[t*3+0]*3+1],verts[tris[t*3+0]*3+2]]);
+        const p1=vertexShader([verts[tris[t*3+1]*3+0],verts[tris[t*3+1]*3+1],verts[tris[t*3+1]*3+2]]);
+        const p2=vertexShader([verts[tris[t*3+2]*3+0],verts[tris[t*3+2]*3+1],verts[tris[t*3+2]*3+2]]);
         const poly = projector.project(
           matrix,
           p0.position,
