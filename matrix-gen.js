2da31866b18693a8baf5951c836847eecef49aa0
Compdog Inc <66779418+Compdog-inc@users.noreply.github.com>
2023-09-22 09:05:26 -0400

    Update matrix-gen.js

--- matrix-gen.js
+++ matrix-gen.js
@@ -6,6 +6,23 @@ window.Utils['_register']('MatrixGen', function(factory){
     const Mscale = function (x, y, z) {
       return [x, 0, 0, 0, 0, y, 0, 0, 0, 0, z, 0, 0, 0, 0, 1];
     };
+    const MrotQ = function(q) {
+      return [
+        2 * (q0 * q0 + q1 * q1) - 1,
+        2 * (q1 * q2 - q0 * q3),
+        2 * (q1 * q3 + q0 * q2),
+        0,
+        2 * (q1 * q2 + q0 * q3),
+        2 * (q0 * q0 + q2 * q2) - 1,
+        2 * (q2 * q3 - q0 * q1),
+        0,
+        2 * (q1 * q3 - q0 * q2),
+        2 * (q2 * q3 + q0 * q1),
+        2 * (q0 * q0 + q3 * q3) - 1,
+        0,
+        0,0,0,1
+      ];
+    };
     const Mrotx = function (a) {
       return [
         1,
@@ -66,6 +83,6 @@ window.Utils['_register']('MatrixGen', function(factory){
         1
       ];
     };
-    factory(id, {Mtranslate,Mscale,Mrot:{x:Mrotx,y:Mroty,z:Mrotz}});
+    factory(id, {Mtranslate,Mscale,Mrot:{x:Mrotx,y:Mroty,z:Mrotz,q:MrotQ}});
   }
 });
